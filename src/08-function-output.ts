08b84640ad946c50b481b62b1e89f9844e7d2d56
Egils <63558651+looppng@users.noreply.github.com>
2023-10-02 18:34:22 +0200

    07-11

--- src/08-function-output.ts
+++ src/08-function-output.ts
@@ -1,8 +1,8 @@
-export {};
+export { };
 
 function getNumber() {
   return Math.random() * 10;
 }
 
-const result = null; // call the function created above and put the result inside the variable
+const result = getNumber(); // call the function created above and put the result inside the variable
 console.log(result); // Expected output: {random number}
--- src/11-function-output.ts
+++ src/11-function-output.ts
@@ -1,7 +1,7 @@
-export {};
+export { };
 
-function concatenate(firstWord, secondWord, thirdWord) {
-  // Write the body of this function to concatenate three words together with space between them
+function concatenate(firstWord: string, secondWord: string, thirdWord: string) {
+  return firstWord.concat(' ').concat(secondWord).concat(' ').concat(thirdWord)
 }
 
 const result = concatenate("Hello", "from", "CODELEX");
